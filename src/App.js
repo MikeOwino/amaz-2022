04399ef4faaf0727964dc2f4d0f2ca46f1880b27
Mike <mike@mikeowino.com>
2022-04-03 17:20:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <video id="my-video" className="video-js" controls preload="auto" poster="https://images.pexels.com/photos/5702341/pexels-photo-5702341.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" data-setup loop>
-        <source src="https://videos-cloudflare.jwpsrv.com/6249d687_e50a61eea3729f6c6ec9123a9539c12c398cd4d6/content/conversions/hmUZMORz/videos/3b4SgphN-27450849.mp4" type="video/mp4" />
+        <source src="https://res.cloudinary.com/weknow-creators/video/upload/v1648995594/3b4SgphN-27450849_u00kiw.mp4" type="video/mp4" />
       </video>
       <Helmet>
       <script
