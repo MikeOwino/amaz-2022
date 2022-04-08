8f2f85556cb5c679a3f55d5930e9463d11f76744
Mike <mike@mikeowino.com>
2022-04-08 15:00:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <video id="my-video" className="video-js" controls preload="auto" poster="https://images.pexels.com/photos/5702341/pexels-photo-5702341.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" data-setup loop>
-        <source src="https://nl20.seedr.cc/ff_get/1142816681/All.the.Old.Knives.2022.1080p.AMZN.WEBRip.1400MB.DD5.1.x264-GalaxyRG.mkv?st=2bAlgF7BRoM9F0EBWbwdyA&e=1649503710" type="video/mp4" />
+        <source src="https://nl20.seedr.cc/ff_get/1142834148/All.the.Old.Knives.2022.720p.AMZN.WEBRip.800MB.x264-GalaxyRG.mkv?st=3ETTMb1g58txQMQ51AzPVg&e=1649505600" type="video/mp4" />
       </video>
       <Helmet>
       <script
